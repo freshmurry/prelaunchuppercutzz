0b4440293c97fe135b902accc5d4a60ea1222ecf
FreshMurry <lawrencemurry@yahoo.com>
2017-07-14 16:20:37 +0000

    Editing images and text

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,36 +13,36 @@ class User < ActiveRecord::Base
   before_create :create_referral_code
   after_create :send_welcome_email
 
-  # REFERRAL_STEPS = [
-  #   {
-  #     'count' => 5,
-  #     'html' => 'Lashes',
-  #     'class' => 'two',
-  #     'image' => ActionController::Base.helpers.asset_path(
-  #       'refer/cream-tooltip@2x.png')
-  #   },
-  #   {
-  #     'count' => 10,
-  #     'html' => 'Makeup',
-  #     'class' => 'three',
-  #     'image' => ActionController::Base.helpers.asset_path(
-  #       'refer/truman@2x.png')
-  #   },
-  #   {
-  #     'count' => 25,
-  #     'html' => 'Eyebrows',
-  #     'class' => 'four',
-  #     'image' => ActionController::Base.helpers.asset_path(
-  #       'refer/winston@2x.png')
-  #   },
-  #   {
-  #     'count' => 50,
-  #     'html' => 'Pamper Me<br>(1) Free Lashes, Makeup, Eyebrows, and Hairstyle service',
-  #     'class' => 'five',
-  #     'image' => ActionController::Base.helpers.asset_path(
-  #       'refer/blade-explain@2x.png')
-  #   }
-  # ]
+  REFERRAL_STEPS = [
+    {
+      'count' => 5,
+      'html' => 'Lashes',
+      'class' => 'two',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/cream-tooltip@2x.png')
+    },
+    {
+      'count' => 10,
+      'html' => 'Makeup',
+      'class' => 'three',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/truman@2x.png')
+    },
+    {
+      'count' => 25,
+      'html' => 'Eyebrows',
+      'class' => 'four',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/winston@2x.png')
+    },
+    {
+      'count' => 50,
+      'html' => '<b>Pamper Me</b><br>(1) Free Lashes, Makeup, Eyebrows, and Hairstyle service',
+      'class' => 'five',
+      'image' => ActionController::Base.helpers.asset_path(
+        'refer/blade-explain@2x.png')
+    }
+  ]
   
   private
 
