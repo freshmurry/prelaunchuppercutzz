4724a34480b7d9915f044b805f1718ce415ced22
FreshMurry <lawrencemurry@yahoo.com>
2017-08-01 20:48:04 +0000

    Fixed port issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ end
 Prelaunchr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 5432 }
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 3000 }
   
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -53,4 +53,9 @@ Prelaunchr::Application.configure do
   # For mailer configs
   config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
+  
+   # For mailer configs
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_MAILER_HOST'] }
 end
