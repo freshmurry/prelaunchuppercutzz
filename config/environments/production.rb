2c593f47c28b9d2777a03511a5b271a199538593
FreshMurry <lawrencemurry@yahoo.com>
2017-08-09 16:32:19 +0000

    Updated Code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,11 +7,8 @@ Prelaunchr::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
-  config.eager_load = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.eager_load = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
